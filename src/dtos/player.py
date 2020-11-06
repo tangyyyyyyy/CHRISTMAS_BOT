e6c9a000ab65fde5b2f38a494a2cb964827a37ee
Frank Poon <frankgpoon@gmail.com>
2020-11-05 22:18:16 -0500

    Add types to DTO structures

--- src/dtos/player.py
+++ src/dtos/player.py
@@ -1,5 +1,5 @@
 class PlayerDto: 
-  def __init__(self, discord_id: int, inventory: list=[], coal_count: int=0):
+  def __init__(self, discord_id: int, inventory: list[str]=[], coal_count: int=0):
     self.discord_id = discord_id
     self.inventory = inventory #list of items
     self.coal_count = coal_count
--- src/dtos/server_config.py
+++ src/dtos/server_config.py
@@ -4,7 +4,7 @@ from constants.config import DEFAULT_DESPAWN_TIME_SECONDS, DEFAULT_SPAWN_RATE_PE
 class ServerConfigDto:
   def __init__(self, despawn_time_seconds: int=DEFAULT_DESPAWN_TIME_SECONDS, 
     spawn_rate_percent: int=DEFAULT_SPAWN_RATE_PERCENT, 
-    enabled_channels: list=[], tree: list=[]):
+    enabled_channels: list[str]=[], tree: list[str]=[]):
     self.enabled_channels = enabled_channels
     self.tree = tree
     self.despawn_time = despawn_time_seconds
