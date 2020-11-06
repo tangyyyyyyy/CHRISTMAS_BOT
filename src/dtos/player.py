dc06da88e293f91e8774f93133bf917ce3568170
Frank Poon <frankgpoon@gmail.com>
2020-11-05 22:43:45 -0500

    Add score attribute to player DTO for DB sorting

--- src/dtos/player.py
+++ src/dtos/player.py
@@ -1,5 +1,7 @@
 class PlayerDto: 
-  def __init__(self, discord_id: int, inventory: list[str]=[], coal_count: int=0):
+  def __init__(self, discord_id: int, inventory: list[str]=[], 
+    coal_count: int=0, score: int=0):
     self.discord_id = discord_id
     self.inventory = inventory #list of items
     self.coal_count = coal_count
+    self.score = len(inventory)
