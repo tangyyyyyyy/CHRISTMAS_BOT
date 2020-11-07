96ef4a28567bb7ae7368c8603970d8ad0bac20b7
Frank Poon <frankgpoon@gmail.com>
2020-11-07 02:48:43 -0500

    Add embeds for enable/disable

--- christmasbot/api/admin.py
+++ christmasbot/api/admin.py
@@ -1,3 +1,6 @@
+import discord
+from constants.globals import ChristmasColor
+
 from constants.messages import BAD_COMMAND_MESSAGE
 from daos.abstract_dao import AbstractDao
 
@@ -6,22 +9,48 @@ async def handle_enable_channel(message, dao: AbstractDao, tokens: list[str]):
   if len(tokens) > 1:
     return BAD_COMMAND_MESSAGE
   if dao.enable_channel(message.guild.id, message.channel.id) == None:
-    response = 'I\'m already enabled on this channel!'
+    response = discord.Embed(
+      title='I\'m already enabled on this channel!',
+      description='Christmas creatures are already coming here!',
+      color=discord.Colour(ChristmasColor.RED)
+    )
   else:
     print('Enabled channel')
-    response = 'ChristmasBot is now enabled on this channel!'
-  await message.channel.send(response) 
+    response = discord.Embed(
+      title='Christmas Bot is now enabled on this channel!',
+      description='Christmas creatures will now start coming here!',
+      color=discord.Colour(ChristmasColor.GREEN)
+    )
+  response.set_author(
+      name='Christmas Bot',
+      icon_url='https://i.imgur.com/YZ6v1jw.png',
+      url='https://github.com/tangyyyyyyy/CHRISTMAS_BOT'
+  )
+  await message.channel.send(embed=response) 
   
 
 async def handle_disable_channel(message, dao: AbstractDao, tokens: list[str]):
   if len(tokens) > 1:
     return BAD_COMMAND_MESSAGE
   if dao.disable_channel(message.guild.id, message.channel.id) == None:
-    response = 'I\'m already disabled on this channel!'
+    response = discord.Embed(
+      title='I\'m already disabled on this channel!',
+      description='Christmas creatures aren\'t coming here already!',
+      color=discord.Colour(ChristmasColor.RED)
+    )
   else:
     print('Disabled channel')
-    response = 'ChristmasBot is now disabled on this channel!'
-  await message.channel.send(response) 
+    response = discord.Embed(
+      title='Christmas Bot is now disabled on this channel!',
+      description='Christmas creatures will no longer come here!',
+      color=discord.Colour(ChristmasColor.GREEN)
+    )
+  response.set_author(
+      name='Christmas Bot',
+      icon_url='https://i.imgur.com/YZ6v1jw.png',
+      url='https://github.com/tangyyyyyyy/CHRISTMAS_BOT'
+  )
+  await message.channel.send(embed=response) 
 
 
 async def handle_change_despawn_time(message, dao: AbstractDao, tokens):
