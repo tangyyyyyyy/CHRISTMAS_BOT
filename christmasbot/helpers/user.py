0e05f7053d4aff52b935cb811548347359751af0
Janina <janinatalvarez@Gmail.com>
2020-11-09 02:05:05 -0800

    added user tags to xnaughty and xnice responses adjusted delete timer fromatted inventory list and separated items into lists by rarity

--- christmasbot/helpers/user.py
+++ christmasbot/helpers/user.py
@@ -6,13 +6,42 @@ import discord
 from constants.globals import ChristmasColor
 from api import user
 
+
+def empty_inventory(user):
+  formatted_string = '\nWait... You have nothing? That\'s so pathe-- I mean, sad...'
+  return discord.Embed(
+    description='**Wow,** ' + user.mention + 
+                '**! Look at all your items:** \n \n'
+                'You own' + ' 0 ' + 'common items \n' + formatted_string  + '\n',
+    color=discord.Colour(ChristmasColor.GOLD)
+).set_footer(
+      text='Page x/x | Use the emotes to switch pages/rarity.'
+)
+
 def format_inventory(user, inventory: [ItemDto]):
+  if len(inventory) == 0:
+    return(empty_inventory(user))
+
+  common_list = []
+  special_list = []
+  rare_list = []
   formatted_string = '\n'
+  
   for item in inventory:
     add_to_formatted_string = item.display_name + '\n'
     formatted_string = formatted_string+add_to_formatted_string
-  if len(inventory) == 0:
-    formatted_string = '\nWait... You have nothing? That\'s so pathe-- I mean, sad...'
-  return discord.Embed(description='Wow,' + user.mention + 
-                      '! Look at all your items: \n' + formatted_string,
-                       color=discord.Colour(ChristmasColor.GOLD))
+    if (item.rarity.name=='COMMON'):
+     common_list.append(item.display_name)
+    elif (item.rarity.name=='SPECIAL'):
+     special_list.append(item.display_name)
+    else:
+     rare_list.append(item.display_name)
+
+  return discord.Embed(
+    description='**Wow,** ' + user.mention + 
+                '**! Look at all your items:** \n \n'
+                'You own' + ' x ' + 'common items \n' + formatted_string  + '\n',
+    color=discord.Colour(ChristmasColor.GOLD)
+).set_footer(
+      text='Page x/x | Use the emotes to switch pages/rarity.'
+)
